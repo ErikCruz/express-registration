c2f6ca2f0cd06ff5a71aa0c825775e9de57b9d01
Erik Cruz <erikc1989@live.com>
2015-06-28 03:35:24 +0000

    Add password hashing with bcrypt

--- models/user.js
+++ models/user.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 mongoose.connect('mongodb://localhost:27017/nodeauth');
 var db = mongoose.connection;
 
 // User Schema
 var UserSchema = mongoose.Schema({
    username: {type: String, index: true},
-   password: {type: String},
+   password: {type: String, bcrypt: true, required: true},
    email: {type: String},
    name: {type: String},
    profileimage: {type: String}
@@ -14,5 +15,11 @@ var UserSchema = mongoose.Schema({
 var User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.createUser = function(newUser, callback){
-    newUser.save(callback);
+    bcrypt.hash(newUser.password, 10, function(err, hash){
+        if(err) throw err;
+        // set hash password
+        newUser.password = hash
+        // create user
+        newUser.save(callback);
+    });
 }
